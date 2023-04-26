3259ee9e6fa47f0c917715bfcc94cad766c96f43
AidenBulwinkle <jo.joe.moe7@gmail.com>
2023-04-25 18:35:37 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -19,3 +19,30 @@ function main() {
 
 // let's get this party started
 main();
+
+//Variables for a car
+var carMake = 'Ford,';
+
+var carModel = 'Tarus,';
+
+var carColor = 'rusty red,';
+
+var carYear = 1995;
+
+var currentYear = 2023;
+
+var carAge = currentYear - carYear;
+
+console.log(carAge);
+
+document.writeln("MAKE: " +carMake)
+
+document.writeln("MODEL: " +carModel)
+
+document.writeln("COLOR: " +carColor)
+
+document.writeln("YEAR: " +carYear)
+
+document.writeln(", HOW OLD: " + carAge + " years old")
+
+
