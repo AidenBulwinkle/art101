4d9967efe4015af7d718b63a5e2b8ae67a78b112
AidenBulwinkle <jo.joe.moe7@gmail.com>
2023-05-09 17:45:43 -0700

    Lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - JavaScript for the Web
+// Author: Aiden Olivier and Elenes Vilchis
+// Date: 5.9.23
 
 // Constants
 
@@ -19,3 +19,25 @@ function main() {
 
 // let's get this party started
 main();
+ 
+ var outputEl = document.getElementById("output");
+ console.log("outputEl: ", outputEl);
+ 
+ var new1El = document.createElement("p");
+
+ new1El.innerHTML = "Thing 1";
+ new1El.id = "new-one";
+ new1El.style.background = "yellow"
+
+ var new2El = document.createElement("p");
+ new1El.id = "new-two";
+
+ new2El.innerHTML = "Thing 1 again but cooler";
+ new2El.style.color = "red"
+ new2El.style.fontFamily = "times-new-roman"
+ new2El.style.background = "black"
+
+ outputEl.appendChild(new1El);
+ outputEl.appendChild(new2El);
+
+ outputEl.className = "changed";
